cee2cd6db5967710b5bf1b24ce878a8b3a09135d
AliJenabi78 <a.jenabi78@gmail.com>
2025-12-13 13:05:07 +0330

    feat: complete exercise 2 - Boxing/Unboxing benchmark

---
+++ src/BoxingUnboxingImpact/BoxingBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections;
+using System.Collections.Generic;
+
+[MemoryDiagnoser]
+public class BoxingBenchmark
+{
+    private const int Operations = 10_000_000;
+    [Benchmark]
+    public void TestList()
+    {
+        List<int> list = new List<int>();
+        int sum = 0;
+        for (int i = 0; i < Operations; i++)
+        {
+            list.Add(i);
+            sum += i;
+        }
+        GC.KeepAlive(list);
+    }
+
+    [Benchmark]
+    public void TestArrayList()
+    {
+        ArrayList arrayList = new ArrayList();
+        int sum = 0;
+        for (int i = 0; i < Operations; i++)
+        {
+            arrayList.Add(i);
+            sum += i;
+        }
+        GC.KeepAlive(arrayList);
+    }
+}
--- src/BoxingUnboxingImpact/Program.cs
+++ src/BoxingUnboxingImpact/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+
+class Pogram
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<BoxingBenchmark>();
+    }
+}
