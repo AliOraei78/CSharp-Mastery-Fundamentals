2decb639980e16ed3f9c61eda7c7bae2c7f24c51
AliJenabi78 <a.jenabi78@gmail.com>
2025-12-13 12:24:01 +0330

    add benchmark results and README for exercise 1

--- src/ValueVsReferencePerformance/Program.cs
+++ src/ValueVsReferencePerformance/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+
+public class CustomerClass
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Balance { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public bool IsActive { get; set; }
+}
+
+public struct CustomerStruct
+{
+    public int Id;
+    public string? Name;
+    public decimal Balance;
+    public DateTime RegistrationDate;
+    public bool IsActive;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<ValueVsReferenceBenchmark>();
+    }
+}
---
+++ src/ValueVsReferencePerformance/ValueVsReferenceBenchmark.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.Diagnostics.Tracing.Parsers.JScript;
+
+[MemoryDiagnoser]
+public class ValueVsReferenceBenchmark
+{
+    private const int Operations = 10_000_000;
+    private CustomerClass _classInstance = new() { Id = 1, Name = "Test", Balance = 1000m, RegistrationDate = DateTime.UtcNow, IsActive = true };
+    private CustomerStruct _structInstance = new() { Id = 1, Name = "Test", Balance = 1000m, RegistrationDate = DateTime.UtcNow, IsActive = true };
+
+    [Benchmark]
+    public void CopyClass()
+    {
+        CustomerClass copy = null;
+        for (int i = 0; i < Operations; i++)
+        {
+            copy = _classInstance;
+        }
+        GC.KeepAlive(copy);
+    }
+    
+    [Benchmark]
+    public void CopyStruct()
+    {
+        CustomerStruct copy = default;
+        for (int i = 0; i < Operations; i++)
+        {
+            copy = _structInstance;
+        }
+        GC.KeepAlive(copy);
+    }
+
+    [Benchmark]
+    public void AllocateListClass()
+    {
+        var list = new List<CustomerClass>(1_000_000);
+        for (int i = 0;i < 1_000_000; i++)
+        {
+            list.Add(new CustomerClass { Id = i });
+        }
+        GC.KeepAlive(list);
+    }
+
+    [Benchmark]
+    public void AllocateListStruct()
+    {
+        var list = new List<CustomerStruct>(1_000_000);
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            list.Add(new CustomerStruct { Id = i });
+        }
+        GC.KeepAlive(list);
+    }
+}
